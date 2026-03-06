1e6b6bc057ed91f069407c97c3a61f9f98e180dc
TMerlini <114340671+TMerlini@users.noreply.github.com>
2026-03-06 00:40:51 +0000

    Reduce login slide spacing to make videos larger

--- push-to-master.ps1
+++ push-to-master.ps1
@@ -6,5 +6,5 @@ $git = "C:\Program Files\Git\cmd\git.exe"
 
 & $git add .
 & $git status
-& $git commit -m "Add unsynced staggered slide animations for login page text and media columns"
+& $git commit -m "Reduce login slide spacing to make videos larger"
 & $git push origin master
