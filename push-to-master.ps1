db5490c5de71a4aeee8b8cd32b1fc9b2c4b43738
TMerlini <114340671+TMerlini@users.noreply.github.com>
2026-03-06 00:34:55 +0000

    Add unsynced staggered slide animations for login page text and media columns

--- push-to-master.ps1
+++ push-to-master.ps1
@@ -6,5 +6,5 @@ $git = "C:\Program Files\Git\cmd\git.exe"
 
 & $git add .
 & $git status
-& $git commit -m "Add slide titles, descriptions, alignment, and YouTube video support to login page and admin images tab"
+& $git commit -m "Add unsynced staggered slide animations for login page text and media columns"
 & $git push origin master
